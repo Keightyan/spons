2fbfcea8dad00a09c9f1d3b5a65f46dbba4da14b
Keightyan <keightyan@gmail.com>
2023-01-15 18:00:50 +0900

    2001151800

--- resources/views/post/search.blade.php
+++ resources/views/post/search.blade.php
@@ -1,10 +1,3 @@
-{{-- @php
-    $week = ['日', '月', '火', '水', '木', '金', '土'];
-    $date = $post->created_at;
-    $day = new DateTime($date);
-    $dow = $day->format('w');
-@endphp --}}
-
 <x-app-layout>
     <div class="footer_wrap">
         <div class="mx-auto px-4 sm:px-6 lg:px-8" style="max-width: 1100px;">
