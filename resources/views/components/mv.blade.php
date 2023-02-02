fb165363e752beada532b6c579f02744162e950e
Keightyan <keightyan@gmail.com>
2023-02-03 00:01:00 +0900

    2302030000

--- resources/views/components/mv.blade.php
+++ resources/views/components/mv.blade.php
@@ -81,6 +81,7 @@
                         @endif
                     </p>
                     <p class="info">
+                        <?php dd($post->user->profile_image); ?>
                         @if ($post->user->profile_image === null)
                             <img src="{{ asset('/profile_image/' . 'user_default.jpg') }}" class="inline mr-2"
                                 style="height: 35px;">
